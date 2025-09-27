81313f046f8e8f61cd9898ad3c576ad5c3105c0c
tasnimbm <bmbarektasnim@gmail.com>
2025-09-27 13:51:20 +0100

    deployment issue fix

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 import { cn } from "@/lib/utils";
-import {call} from "dayjs";
+import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
 
 enum CallStatus {
     INACTIVE = "INACTIVE",
@@ -10,81 +15,173 @@ enum CallStatus {
     FINISHED = "FINISHED",
 }
 
-const Agent = ({ userName }: AgentProps) => {
-    const isSpeaking = true;
-    const callStatus = CallStatus.FINISHED; // 👈 simulate for testing
+interface SavedMessage {
+    role: "user" | "system" | "assistant";
+    content: string;
+}
 
-    const messages= [
-        'Whats your name ?',
-        'My name is john doe, nice to meet you!'
-    ];
-    const lastmessage = messages[messages.length-1];
+const Agent = ({
+                   userName,
+                   userId,
+                   interviewId,
+                   feedbackId,
+                   type,
+                   questions,
+               }: AgentProps) => {
+    const router = useRouter();
+    const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+    const [messages, setMessages] = useState<SavedMessage[]>([]);
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const [lastMessage, setLastMessage] = useState<string>("");
+
+    useEffect(() => {
+        const onCallStart = () => {
+            setCallStatus(CallStatus.ACTIVE);
+        };
+
+        const onCallEnd = () => {
+            setCallStatus(CallStatus.FINISHED);
+        };
+
+        const onMessage = (message: Message) => {
+            if (message.type === "transcript" && message.transcriptType === "final") {
+                const newMessage = { role: message.role, content: message.transcript };
+                setMessages((prev) => [...prev, newMessage]);
+            }
+        };
+
+        const onSpeechStart = () => {
+            console.log("speech start");
+            setIsSpeaking(true);
+        };
+
+        const onSpeechEnd = () => {
+            console.log("speech end");
+            setIsSpeaking(false);
+        };
+
+        const onError = (error: Error) => {
+            console.log("Error:", error);
+        };
+
+        vapi.on("call-start", onCallStart);
+        vapi.on("call-end", onCallEnd);
+        vapi.on("message", onMessage);
+        vapi.on("speech-start", onSpeechStart);
+        vapi.on("speech-end", onSpeechEnd);
+        vapi.on("error", onError);
+
+        return () => {
+            vapi.off("call-start", onCallStart);
+            vapi.off("call-end", onCallEnd);
+            vapi.off("message", onMessage);
+            vapi.off("speech-start", onSpeechStart);
+            vapi.off("speech-end", onSpeechEnd);
+            vapi.off("error", onError);
+        };
+    }, []);
+
+
+    const handleCall = async () => {
+        setCallStatus(CallStatus.CONNECTING);
+
+        if (type === "generate") {
+            await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+                variableValues: {
+                    username: userName,
+                    userid: userId,
+                },
+            });
+        } else {
+            let formattedQuestions = "";
+            if (questions) {
+                formattedQuestions = questions
+                    .map((question) => `- ${question}`)
+                    .join("\n");
+            }
+
+            await vapi.start(interviewer, {
+                variableValues: {
+                    questions: formattedQuestions,
+                },
+            });
+        }
+    };
+
+    const handleDisconnect = () => {
+        setCallStatus(CallStatus.FINISHED);
+        vapi.stop();
+    };
+
     return (
         <>
             <div className="call-view">
+                {/* AI Interviewer Card */}
                 <div className="card-interviewer">
                     <div className="avatar">
                         <Image
                             src="/ai-avatar.png"
-                            alt="vapi"
+                            alt="profile-image"
                             width={65}
                             height={54}
                             className="object-cover"
                         />
-
                         {isSpeaking && <span className="animate-speak" />}
                     </div>
                     <h3>AI Interviewer</h3>
                 </div>
 
+                {/* User Profile Card */}
                 <div className="card-border">
                     <div className="card-content">
                         <Image
                             src="/user-avatar.png"
-                            alt="user avatar"
+                            alt="profile-image"
+                            width={539}
+                            height={539}
                             className="rounded-full object-cover size-[120px]"
-                            width={540}
-                            height={540}
                         />
                         <h3>{userName}</h3>
                     </div>
                 </div>
             </div>
 
-            {messages.length >0 &&(
+            {messages.length > 0 && (
                 <div className="transcript-border">
                     <div className="transcript">
-                        <p key={lastmessage} className={cn(
-                            'transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100'
-
-                        )}>
-                            {lastmessage}
-
+                        <p
+                            key={lastMessage}
+                            className={cn(
+                                "transition-opacity duration-500 opacity-0",
+                                "animate-fadeIn opacity-100"
+                            )}
+                        >
+                            {lastMessage}
                         </p>
-
                     </div>
-
                 </div>
             )}
 
             <div className="w-full flex justify-center">
-                {callStatus !== 'ACTIVE' ? (
-                    <button className="relative btn-call">
-                        <span
-                            className={cn(
-                                "absolute animate-ping rounded-full opacity-75",
-                                callStatus !== 'CONNECTING' & "hidden"
-                            )}
+                {callStatus !== "ACTIVE" ? (
+                    <button className="relative btn-call" onClick={() => handleCall()}>
+            <span
+                className={cn(
+                    "absolute animate-ping rounded-full opacity-75",
+                    callStatus !== "CONNECTING" && "hidden"
+                )}
+            />
 
-                            />
-
-
-                        <span>
-                            {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call': '. . .'}
-                        </span>
+                        <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                  ? "Call"
+                  : ". . ."}
+            </span>
                     </button>
                 ) : (
-                    <button className="btn-disconnect">End</button>
+                    <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+                        End
+                    </button>
                 )}
             </div>
         </>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,92 +1,23 @@
-'use server';
+"use server";
 
-import { db, auth } from "@/firebase/admin";
+import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-const ONE_WEEK = 60 * 60 * 24 * 7 * 1000; // milliseconds
+// Session duration (1 week)
+const SESSION_DURATION = 60 * 60 * 24 * 7;
 
-// ---------------- SIGN UP ----------------
-export async function signUp(params: SignUpParams) {
-    const { uid, name, email } = params;
-
-    try {
-        // Get user document by UID
-        const userDoc = await db.collection("users").doc(uid).get();
-
-        if (userDoc.exists) {
-            return {
-                success: false,
-                message: "User already exists. Please sign in instead.",
-            };
-        }
-
-        // Create the user document
-        await db.collection("users").doc(uid).set({
-            name,
-            email,
-        });
-
-        return {
-            success: true,
-            message: "Account created successfully. Please sign in.",
-        };
-    } catch (e: any) {
-        console.error("Error creating a user:", e);
-
-        if (e.code === "auth/email-already-exists") {
-            return {
-                success: false,
-                message: "This email is already in use.",
-            };
-        }
-
-        return {
-            success: false,
-            message: "Failed to create account.",
-        };
-    }
-}
-
-// ---------------- SIGN IN ----------------
-export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
-
-    try {
-        const userRecord = await auth.getUserByEmail(email);
-
-        if (!userRecord) {
-            return {
-                success: false,
-                message: "User does not exist. Create an account instead.",
-            };
-        }
-
-        // Set session cookie
-        await setSessionCookie(idToken);
-
-        return {
-            success: true,
-            message: "Signed in successfully.",
-        };
-    } catch (e: any) {
-        console.error("Error logging in:", e);
-        return {
-            success: false,
-            message: "Failed to log into an account.",
-        };
-    }
-}
-
-// ---------------- SESSION COOKIE ----------------
+// Set session cookie
 export async function setSessionCookie(idToken: string) {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
 
+    // Create session cookie
     const sessionCookie = await auth.createSessionCookie(idToken, {
-        expiresIn: ONE_WEEK, // milliseconds
+        expiresIn: SESSION_DURATION * 1000, // milliseconds
     });
 
+    // Set cookie in the browser
     cookieStore.set("session", sessionCookie, {
-        maxAge: ONE_WEEK / 1000, // maxAge is in seconds
+        maxAge: SESSION_DURATION,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         path: "/",
@@ -94,34 +25,107 @@ export async function setSessionCookie(idToken: string) {
     });
 }
 
-// ---------------- GET CURRENT USER ----------------
-export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = cookies();
-    const sessionCookie = cookieStore.get("session")?.value;
+export async function signUp(params: SignUpParams) {
+    const { uid, name, email } = params;
 
+    try {
+        // check if user exists in db
+        const userRecord = await db.collection("users").doc(uid).get();
+        if (userRecord.exists)
+            return {
+                success: false,
+                message: "User already exists. Please sign in.",
+            };
+
+        // save user to db
+        await db.collection("users").doc(uid).set({
+            name,
+            email,
+            // profileURL,
+            // resumeURL,
+        });
+
+        return {
+            success: true,
+            message: "Account created successfully. Please sign in.",
+        };
+    } catch (error: any) {
+        console.error("Error creating user:", error);
+
+        // Handle Firebase specific errors
+        if (error.code === "auth/email-already-exists") {
+            return {
+                success: false,
+                message: "This email is already in use",
+            };
+        }
+
+        return {
+            success: false,
+            message: "Failed to create account. Please try again.",
+        };
+    }
+}
+
+export async function signIn(params: SignInParams) {
+    const { email, idToken } = params;
+
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+        if (!userRecord)
+            return {
+                success: false,
+                message: "User does not exist. Create an account.",
+            };
+
+        await setSessionCookie(idToken);
+    } catch (error: any) {
+        console.log("");
+
+        return {
+            success: false,
+            message: "Failed to log into account. Please try again.",
+        };
+    }
+}
+
+// Sign out user by clearing the session cookie
+export async function signOut() {
+    const cookieStore = await cookies();
+
+    cookieStore.delete("session");
+}
+
+// Get current user from session cookie
+export async function getCurrentUser(): Promise<User | null> {
+    const cookieStore = await cookies();
+
+    const sessionCookie = cookieStore.get("session")?.value;
     if (!sessionCookie) return null;
 
     try {
         const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-        const userDoc = await db
+        // get user info from db
+        const userRecord = await db
             .collection("users")
-            .doc(decodedClaims.uid) // use uid, not a second argument
+            .doc(decodedClaims.uid)
             .get();
-
-        if (!userDoc.exists) return null;
+        if (!userRecord.exists) return null;
 
         return {
-            ...userDoc.data(),
-            id: userDoc.id,
+            ...userRecord.data(),
+            id: userRecord.id,
         } as User;
-    } catch (e) {
-        console.error("Error getting current user:", e);
+    } catch (error) {
+        console.log(error);
+
+        // Invalid or expired session
         return null;
     }
 }
 
-// ---------------- CHECK AUTH ----------------
+// Check if user is authenticated
 export async function isAuthenticated() {
     const user = await getCurrentUser();
     return !!user;
